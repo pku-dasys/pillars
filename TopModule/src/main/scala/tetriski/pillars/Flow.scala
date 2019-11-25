580f3fea248dbee07f9c7334160b6cfc912bbe1a
g478227411 <478227411@qq.com>
2019-11-25 16:40:04 +0800

    modify some remarks

--- TopModule/src/main/scala/tetriski/pillars/Flow.scala
+++ TopModule/src/main/scala/tetriski/pillars/Flow.scala
@@ -10,20 +10,22 @@ object Flow {
     //Function: ((a+b)*a+b)?(a)
     //? is select by config
 
-    val moduleNum = List(2,1,1)
+    val moduleNum = List(2, 1, 1)
 
-    val maplist = Map(List(0, 0 ,0) -> List(List(1, 0, 1)),
-      List(3, 0 ,2) -> List(List(1, 0, 2), List(0, 0, 2), List(2, 0, 1)),
-      List(3, 0 ,1) -> List(List(0, 0, 1), List(0, 1, 2)),
+    val maplist = Map(List(0, 0, 0) -> List(List(1, 0, 1)),
+      List(3, 0, 2) -> List(List(1, 0, 2), List(0, 0, 2), List(2, 0, 1)),
+      List(3, 0, 1) -> List(List(0, 0, 1), List(0, 1, 2)),
       List(1, 0, 0) -> List(List(0, 1, 1)),
-      List(0, 1 ,0) -> List(List(2, 0, 2)),
-      List(2, 0 ,0) -> List(List(3, 0, 0))
+      List(0, 1, 0) -> List(List(2, 0, 2)),
+      List(2, 0, 0) -> List(List(3, 0, 0))
     )
 
     val moduleInfo = List(moduleNum, List(16, 32, 16, 32))
-//    val topmodule = Module(new TopMo())
-    chisel3.Driver.execute(args, () => new TopModule(moduleInfo, maplist, 32))  //verilog generation
 
+    //Verilog generation
+    chisel3.Driver.execute(args, () => new TopModule(moduleInfo, maplist, 32))
+
+    //Run tester
     iotesters.Driver.execute(args, () => new TopModule(moduleInfo, maplist, 32)) {
       c => new TopModuleUnitTest(c)
     }
