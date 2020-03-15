65f5dbcc47e98b70cc4ec4cec65cace642ab3773
jaryx_cat <1400011389@pku.edu.cn>
2020-03-15 23:04:05 +0800

    Update testILP

--- TopModule/src/main/scala/tetriski/pillars/Mapping/testILP.scala
+++ TopModule/src/main/scala/tetriski/pillars/Mapping/testILP.scala
@@ -3,51 +3,68 @@ package tetriski.pillars.Mapping
 import tetriski.pillars.core._
 import java.io._
 object testILP {
-  var dfgnames = Array(//"accum", "cap", "conv2", "conv3", "mac", "mac2",
-    //"matrixmultiply", "mults1", "mults2", "nomem1",
+  var dfgnames = Array("accum", "cap", "conv2", "conv3", "mac", "mac2",
+    "matrixmultiply", "mults1", "mults2", "nomem1",
     "simple", "simple2", "sum")
+//  dfgnames = Array("mults1", "mults2", "nomem1",
+//    "simple", "simple2", "sum")
 
   var mrrgnames = Array("full", "reduce", "simple", "full-simple")
+//  mrrgnames = Array("simple", "reduce")
   def main(args: Array[String]) = {
-    val fw = new FileWriter("test_mapping_result.txt")
-    fw.write("CGRA-ME:\n")
-    for (mrrgname <- mrrgnames) {
+    val fw_c = new FileWriter("cgrame_mapping_result.txt")
+    val fw_p = new FileWriter("pillars_mapping_result.txt")
+//    fw_c.write("CGRA-ME:\n")
+//    for (mrrgname <- mrrgnames) {
+//      for (i <- 1 to 2) {
+//        val mrrg = new MRRG()
+//        mrrg.loadtxt("MRRG_CGRAME/cgra-mrrg-4m4-ii" + i + "-" + mrrgname + ".txt")
+//        for (dfgname <- dfgnames) {
+//          val dfg = DotReader.loadDot("DOT_CGRAME/" + dfgname + ".dot")
+//          println("mapping " + dfgname + " to arch " + mrrgname + " ii = " + i)
+//          val time = ILPMap.mapping(dfg, mrrg, fw_c = fw_c)
+//          if(time == -1) {
+//            fw_c.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " fail")
+//          } else {
+//            fw_c.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " success, time = " + time)
+//          }
+//          fw_c.write("\n")
+//          fw_c.flush()
+//        }
+//      }
+//    }
+    fw_p.write("Pillars:\n")
+    fw_c.write("CGRA-ME:\n")
+
       for (i <- 1 to 2) {
-        val mrrg = new MRRG()
-        mrrg.loadtxt("MRRG_CGRAME/cgra-mrrg-4m4-ii" + i + "-" + mrrgname + ".txt")
         for (dfgname <- dfgnames) {
           val dfg = DotReader.loadDot("DOT_CGRAME/" + dfgname + ".dot")
-          println("mapping " + dfgname + " to arch " + mrrgname + " ii = " + i)
-          val time = ILPMap.mapping(dfg, mrrg, fw = fw)
-          if(time == -1) {
-            fw.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " fail")
+          for (mrrgname <- mrrgnames) {
+            val mrrg_p = new MRRG()
+            val mrrg_c = new MRRG()
+            mrrg_p.loadtxt("MRRG/cgra-mrrg-4m4-ii" + i + "-" + mrrgname + ".txt")
+            mrrg_c.loadtxt("MRRG_CGRAME/cgra-mrrg-4m4-ii" + i + "-" + mrrgname + ".txt")
+          val time_p = ILPMap.mapping(dfg, mrrg_p, fw = fw_p)
+          if(time_p == -1) {
+            fw_p.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " fail")
           } else {
-            fw.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " success, time = " + time)
+            fw_p.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " success, time = " + time_p)
           }
-          fw.write("\n")
-          fw.flush()
+          fw_p.write("\n")
+          fw_p.flush()
+
+            val time_c = ILPMap.mapping(dfg, mrrg_c, fw = fw_c)
+            if(time_c == -1) {
+              fw_c.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " fail")
+            } else {
+              fw_c.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " success, time = " + time_c)
+            }
+            fw_c.write("\n")
+            fw_c.flush()
         }
       }
     }
-    fw.write("Pillars:\n")
-    for (mrrgname <- mrrgnames) {
-      for (i <- 1 to 2) {
-        val mrrg = new MRRG()
-        mrrg.loadtxt("MRRG/cgra-mrrg-4m4-ii" + i + "-" + mrrgname + ".txt")
-        for (dfgname <- dfgnames) {
-          val dfg = DotReader.loadDot("DOT_CGRAME/" + dfgname + ".dot")
-          println("mapping " + dfgname + " to arch " + mrrgname + " ii = " + i)
-          val time = ILPMap.mapping(dfg, mrrg, fw = fw)
-          if(time == -1) {
-            fw.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " fail")
-          } else {
-            fw.write(mrrgname + " ii = " + i + " dfg = " + dfgname + " success, time = " + time)
-          }
-          fw.write("\n")
-          fw.flush()
-        }
-      }
-    }
-    fw.close()
+    fw_c.close()
+    fw_p.close()
   }
 }
