8e869f7052217ff0139edf3e3d6a28e082e91eac
g478227411 <478227411@qq.com>
2020-04-03 17:53:05 +0800

    Update ADL

--- TopModule/src/main/scala/tetriski/pillars/Pillars.scala
+++ TopModule/src/main/scala/tetriski/pillars/Pillars.scala
@@ -1,13 +1,16 @@
 package tetriski.pillars
+import tetriski.pillars.archlib.BlockChain
+import tetriski.pillars.core.ArchitctureHierarchy
 import tetriski.pillars.examples.{ApplicationExamples, HardwareExamples}
 
 object Pillars {
   def main(args: Array[String]): Unit = {
 
+    HardwareExamples.exampleBlockChain()
     HardwareExamples.exampleAdres()
     HardwareExamples.exampleLSUAdres()
     HardwareExamples.exampleCompleteAdres()
-//
+
 //    ApplicationExamples.dumpWrapperVerilog()
 //    ApplicationExamples.dumpArch(2, "MRRG/cgra-mrrg-4m4-ii2.txt")
 
