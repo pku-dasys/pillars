ef0601b67e5bbeea5508a759e9ef0e80658d2ba0
Guojie Luo <gluo@pku.edu.cn>
2022-07-16 17:11:27 +0800

    Add Gurobi library files to pass compilation.

--- lib/README.md
+++ lib/README.md
@@ -1,3 +1,5 @@
 ## Third-party Library
 
-``com.microsoft.z3.jar`` and ``libz3java.so`` are from [z3-4.8.16-x64-glibc-2.31.zip](https://github.com/Z3Prover/z3/releases/download/z3-4.8.16/z3-4.8.16-x64-glibc-2.31.zip), redistributed under [MIT License](LICENSE.z3)
+Library files ``com.microsoft.z3.jar`` and ``libz3java.so`` are from [z3-4.8.16-x64-glibc-2.31.zip](https://github.com/Z3Prover/z3/releases/download/z3-4.8.16/z3-4.8.16-x64-glibc-2.31.zip), redistributed under [MIT License](LICENSE.z3).
+
+Library files ``gurobi.jar``, ``libGurobiJni95.so``, and ``libgurobi95.so`` are from [Gurobi 9.5.0](https://packages.gurobi.com/9.5/gurobi9.5.0_linux64.tar.gz). Please find the Gurobi license in [licensing options](https://www.gurobi.com/products/licensing-options/) or its [academic program](https://www.gurobi.com/academia/academic-program-and-licenses/).
--- src/main/scala/tetriski/pillars/mapping/Z3ProverMap.scala
+++ src/main/scala/tetriski/pillars/mapping/Z3ProverMap.scala
@@ -43,7 +43,7 @@ class Z3ProverMap {
   val mrrgRoutFaninId = ArrayBuffer[ArrayBuffer[Int]]()
   val mrrgRoutFanoutId = ArrayBuffer[ArrayBuffer[Int]]()
 
-  val mrrgLatency = Map[String,Int]()
+  val mrrgLatency = Map[String, Int]()
   val mrrgFuncOpcodes = ArrayBuffer[ArrayBuffer[Int]]()
 
   val MRRG_ROUT_NODE_TYPE = 0
@@ -77,21 +77,28 @@ class Z3ProverMap {
     println(opt.getModel())
   }
 
-  def getOmtModel(ctx: Context, separatedPR: Boolean, scheduleControl: Boolean): Array[Optimize] = {
+  def getOmtModel(
+      ctx: Context,
+      separatedPR: Boolean,
+      scheduleControl: Boolean
+  ): Array[Optimize] = {
     val constrPlace = ArrayBuffer[BoolExpr]()
     val constrRoute = ArrayBuffer[BoolExpr]()
 
-    val varF = Map[(Int,Int), IntExpr]() // funcNode variables
-    val varR = Map[(Int,Int), IntExpr]() // routNode variables
-    val varS = Map[(Int,Int,Int), IntExpr]() // path variables
+    val varF = Map[(Int, Int), IntExpr]() // funcNode variables
+    val varR = Map[(Int, Int), IntExpr]() // routNode variables
+    val varS = Map[(Int, Int, Int), IntExpr]() // path variables
 
     // initialize F(*,*)
-    for ((funcNodeName, funcNodeId) <- mrrgFuncName.zipWithIndex; (opNodeName, opNodeId) <- dfgOpNodeName.zipWithIndex) {
-      val isLegal = if (mrrgFuncOpcodes(funcNodeId).indexOf(dfgOpNodeOpcode(opNodeId)) >= 0) {
-        if (fixedMapRelation.contains(opNodeId)) {
-          if (fixedMapRelation(opNodeId).contains(funcNodeId)) true else false
+    for ((funcNodeName, funcNodeId) <- mrrgFuncName.zipWithIndex;
+         (opNodeName, opNodeId) <- dfgOpNodeName.zipWithIndex) {
+      val isLegal =
+        if (mrrgFuncOpcodes(funcNodeId)
+              .indexOf(dfgOpNodeOpcode(opNodeId)) >= 0) {
+          if (fixedMapRelation.contains(opNodeId)) {
+            if (fixedMapRelation(opNodeId).contains(funcNodeId)) true else false
           } else true
-      } else false
+        } else false
       if (isLegal) { // implicit constraits of functional legality
         val name = s"F( ${funcNodeName}, ${opNodeName} )"
         val v = ctx.mkIntConst(name)
@@ -99,7 +106,8 @@ class Z3ProverMap {
       }
     }
     // initialize R(*,*)
-    for ((routNodeName, routNodeId) <- mrrgRoutName.zipWithIndex; (valNodeName, valNodeId) <- dfgValNodeName.zipWithIndex) {
+    for ((routNodeName, routNodeId) <- mrrgRoutName.zipWithIndex;
+         (valNodeName, valNodeId) <- dfgValNodeName.zipWithIndex) {
       val name = s"R( ${routNodeName}, ${valNodeName} )"
       val v = ctx.mkIntConst(name)
       varR.put((routNodeId, valNodeId), v)
@@ -109,18 +117,17 @@ class Z3ProverMap {
         val opNodeName = dfgOpNodeName(opNodeId)
         val name = s"S( ${routNodeName}, ${valNodeName}, ${opNodeName} )"
         val v = ctx.mkIntConst(name)
-        varS.put((routNodeId,valNodeId,opNodeId), v)
+        varS.put((routNodeId, valNodeId, opNodeId), v)
       }
     }
 
+    val zero = ctx.mkInt(0)
+    val one = ctx.mkInt(1)
+
     def bound[T](varList: Map[T, IntExpr]): Iterable[BoolExpr] = {
-      val zero = ctx.mkInt(0)
-      val one = ctx.mkInt(1)
-      varList.map{
-        // v in {0,1}
-        case (_,v) => ctx.mkAnd(
-          ctx.mkLe(zero, v),
-          ctx.mkLe(v, one))
+      varList.map {
+        // bound v in {0,1}
+        case (_, v) => ctx.mkAnd(ctx.mkLe(zero, v), ctx.mkLe(v, one))
       }
     }
     constrPlace.appendAll(bound(varF))
@@ -157,100 +164,147 @@ class Z3ProverMap {
     solver.MkMinimize(ctx.mkInt(0))
     (constrPlace ++ constrRoute).foreach(solver.Add(_))
     logger.trace(solver.toString())
+
     logger.debug(solver.Check())
-    logger.debug(solver.getModel())
+
+    //logger.debug(solver.getModel())
+    val model = solver.getModel()
+    def logVar(v: IntExpr) = {
+      val c = model.getConstInterp(v)
+      if (c.equals(one)) {
+        logger.debug(s"${c} = ${v.getFuncDecl().getName()}")
+      }
+    }
+    for ((_, v) <- varF) logVar(v)
+    for ((_, v) <- varR) logVar(v)
+    for ((_, v) <- varS) logVar(v)
 
     Array(solver, solver) // hack
   }
 
-  def constrFunctionExclusivity(ctx: Context, varF: Map[(Int,Int), IntExpr]): Iterable[BoolExpr] = {
+  def constrFunctionExclusivity(
+      ctx: Context,
+      varF: Map[(Int, Int), IntExpr]
+  ): Iterable[BoolExpr] = {
     val one = ctx.mkInt(1)
-    varF.groupBy{case ((funcNodeId,_),_) => funcNodeId}.flatMap{case (_,vf) => {
-      val varList = vf.map(_._2).toSeq
-      if (varList.size > 0) {
-        val sum = ctx.mkAdd(varList: _*)
-        // every MRRG funcNode is used at most once:
-        //   sum_* F(funcNodeId,*) <= 1
-        Some(ctx.mkLe(sum, one))
-      } else {
-        None
-      }
-    }}
-  }
-
-  def constrOperationPlacement(ctx: Context, varF: Map[(Int,Int), IntExpr]): Iterable[BoolExpr] = {
-    val one = ctx.mkInt(1)
-    varF.groupBy{case ((_,opNodeId),_) => opNodeId}.map{case (_,vf) => {
-      val varList = vf.map(_._2).toSeq
-      assert(varList.size > 0)
-      val sum = ctx.mkAdd(varList: _*)
-      // every DFG opNode is placed exactly once:
-      //   sum_* F(*,opNodeId) == 1
-      ctx.mkEq(sum, one)
-    }}
-  }
-
-  def constrRoutingResource(ctx: Context, varR: Map[(Int,Int), IntExpr], varS: Map[(Int,Int,Int), IntExpr]): Iterable[BoolExpr] = {
-    val vs = varS.groupBy{case ((x,y,_),_) => (x,y)}
-    varR.flatMap{
-      case ((routNodeId, valNodeId), r) => {
-        vs((routNodeId, valNodeId)).map{case (_, s) =>
-          // path (valNode ~> opNode) implies MRRG routNode usage:
-          //   S(routeNodeId,valNodeId,*) <= R(routeNodeId,valNodeId)
-          ctx.mkLe(s, r)
+    varF.groupBy { case ((funcNodeId, _), _) => funcNodeId }.flatMap {
+      case (_, vf) => {
+        val varList = vf.map(_._2).toSeq
+        if (varList.size > 0) {
+          val sum = ctx.mkAdd(varList: _*)
+          // every MRRG funcNode is used at most once:
+          //   sum_* F(funcNodeId,*) <= 1
+          Some(ctx.mkLe(sum, one))
+        } else {
+          None
         }
       }
     }
   }
 
-  def constrRoutingExclusivity(ctx: Context, varR: Map[(Int,Int), IntExpr]): Iterable[BoolExpr] = {
+  def constrOperationPlacement(
+      ctx: Context,
+      varF: Map[(Int, Int), IntExpr]
+  ): Iterable[BoolExpr] = {
     val one = ctx.mkInt(1)
-    varR.groupBy{case ((routNodeId,_),_) => routNodeId}.flatMap{case (_,vr) => {
-      val varList = vr.map(_._2).toSeq
-      if (varList.size > 0) {
+    varF.groupBy { case ((_, opNodeId), _) => opNodeId }.map {
+      case (_, vf) => {
+        val varList = vf.map(_._2).toSeq
+        assert(varList.size > 0)
         val sum = ctx.mkAdd(varList: _*)
-        // every MRRG routNode is used at most once:
-        //   sum_* R(routNodeId,*) <= 1
-        Some(ctx.mkLe(sum, one))
-      } else {
-        None
+        // every DFG opNode is placed exactly once:
+        //   sum_* F(*,opNodeId) == 1
+        ctx.mkEq(sum, one)
       }
-    }}
+    }
   }
 
-  def constrMultiplexerExclusivity(ctx: Context, varR: Map[(Int,Int), IntExpr]): Iterable[BoolExpr] = {
-    val one = ctx.mkInt(1)
-    varR.flatMap{case ((routNodeId, valNodeId), r) => {
-      val faninRoutNodeIds = mrrgRoutFaninId(routNodeId).filter(mrrgRoutFaninType(_) == MRRG_ROUT_NODE_TYPE)
-      if (faninRoutNodeIds.size > 1) {
-        val varList = faninRoutNodeIds.map(i => varR((i,valNodeId)))
-        val sum = ctx.mkAdd(varList: _*)
-        Seq(
-          // active routNode imples active fan-in routNodes:
-          //   R(routNodeId, valNodeId) <= sum_{* in fanin(routeNodeId)} R(*, valNodeId)
-          ctx.mkLe(r, sum),
-          // no more than one active fan-in routNodes for the same valNode:
-          //   (may happen when a value from one MUX input loops back to the other MUX input)
-          //   sum_{* in fanin(routeNodeId)} R(*, valNodeId) <= 1
-          ctx.mkLe(sum, one))
-      } else {
-        None
+  def constrRoutingResource(
+      ctx: Context,
+      varR: Map[(Int, Int), IntExpr],
+      varS: Map[(Int, Int, Int), IntExpr]
+  ): Iterable[BoolExpr] = {
+    val vs = varS.groupBy { case ((x, y, _), _) => (x, y) }
+    varR.flatMap {
+      case ((routNodeId, valNodeId), r) => {
+        vs((routNodeId, valNodeId)).map {
+          case (_, s) =>
+            // path (valNode ~> opNode) implies MRRG routNode usage:
+            //   S(routeNodeId,valNodeId,*) <= R(routeNodeId,valNodeId)
+            ctx.mkLe(s, r)
+        }
       }
-    }}
+    }
   }
 
-  def constrFanoutRouting(ctx: Context, varS: Map[(Int,Int,Int), IntExpr], varF: Map[(Int,Int), IntExpr]): Iterable[BoolExpr] = {
+  def constrRoutingExclusivity(
+      ctx: Context,
+      varR: Map[(Int, Int), IntExpr]
+  ): Iterable[BoolExpr] = {
+    val one = ctx.mkInt(1)
+    varR.groupBy { case ((routNodeId, _), _) => routNodeId }.flatMap {
+      case (_, vr) => {
+        val varList = vr.map(_._2).toSeq
+        if (varList.size > 0) {
+          val sum = ctx.mkAdd(varList: _*)
+          // every MRRG routNode is used at most once:
+          //   sum_* R(routNodeId,*) <= 1
+          Some(ctx.mkLe(sum, one))
+        } else {
+          None
+        }
+      }
+    }
+  }
+
+  def constrMultiplexerExclusivity(
+      ctx: Context,
+      varR: Map[(Int, Int), IntExpr]
+  ): Iterable[BoolExpr] = {
+    val one = ctx.mkInt(1)
+    varR.flatMap {
+      case ((routNodeId, valNodeId), r) => {
+        val faninRoutNodeIds = mrrgRoutFaninId(routNodeId).filter(
+          mrrgRoutFaninType(_) == MRRG_ROUT_NODE_TYPE
+        )
+        if (faninRoutNodeIds.size > 1) {
+          val varList = faninRoutNodeIds.map(i => varR((i, valNodeId)))
+          val sum = ctx.mkAdd(varList: _*)
+          Seq(
+            // active routNode imples active fan-in routNodes:
+            //   R(routNodeId, valNodeId) <= sum_{* in fanin(routeNodeId)} R(*, valNodeId)
+            ctx.mkLe(r, sum),
+            // no more than one active fan-in routNodes for the same valNode:
+            //   (may happen when a value from one MUX input loops back to the other MUX input)
+            //   sum_{* in fanin(routeNodeId)} R(*, valNodeId) <= 1
+            ctx.mkLe(sum, one)
+          )
+        } else {
+          None
+        }
+      }
+    }
+  }
+
+  def constrFanoutRouting(
+      ctx: Context,
+      varS: Map[(Int, Int, Int), IntExpr],
+      varF: Map[(Int, Int), IntExpr]
+  ): Iterable[BoolExpr] = {
     val exprList = ArrayBuffer[BoolExpr]()
     for (routNodeId <- mrrgRoutName.indices; valNodeId <- dfgValNodeName.indices) {
       for ((opNodeId, i) <- dfgValNodeOut(valNodeId).zipWithIndex) {
         val operand = dfgValNodeOutputOperand(valNodeId)(i) // what is operand?
         val varList = ArrayBuffer[IntExpr]()
-        for ((fanoutId,j) <- mrrgRoutFanoutId(routNodeId).zipWithIndex) {
+        for ((fanoutId, j) <- mrrgRoutFanoutId(routNodeId).zipWithIndex) {
           mrrgRoutFanoutType(routNodeId)(j) match {
-            case MRRG_ROUT_NODE_TYPE => varList.append(varS((fanoutId, valNodeId, opNodeId)))
+            case MRRG_ROUT_NODE_TYPE =>
+              varList.append(varS((fanoutId, valNodeId, opNodeId)))
             case MRRG_FUNC_NODE_TYPE => {
               if (mrrgFuncFaninId(fanoutId).size > operand) {
-                if (dfgCommutativeSet.contains(opNodeId) || mrrgFuncFaninId(fanoutId)(operand) == routNodeId) {
+                if (dfgCommutativeSet.contains(opNodeId) || mrrgFuncFaninId(
+                      fanoutId
+                    )(operand) == routNodeId) {
                   if (varF.contains((fanoutId, opNodeId))) {
                     varList.append(varF((fanoutId, opNodeId)))
                   }
@@ -261,28 +315,39 @@ class Z3ProverMap {
         }
         if (varList.size > 0) {
           val sum = ctx.mkAdd(varList: _*)
-          exprList.append(ctx.mkLe(varS((routNodeId, valNodeId, opNodeId)), sum))
+          exprList.append(
+            ctx.mkLe(varS((routNodeId, valNodeId, opNodeId)), sum)
+          )
         }
       }
     }
     exprList
   }
 
-  def constrInitialFanout(ctx: Context, varS: Map[(Int,Int,Int), IntExpr], varF: Map[(Int,Int), IntExpr]): Iterable[BoolExpr] = {
+  def constrInitialFanout(
+      ctx: Context,
+      varS: Map[(Int, Int, Int), IntExpr],
+      varF: Map[(Int, Int), IntExpr]
+  ): Iterable[BoolExpr] = {
     val exprList = ArrayBuffer[BoolExpr]()
-    varF.foreach{case ((funcNodeId,opNodeId),f) => {
-      if (dfgOpNodeOut(opNodeId) != -1) {
-        val valNodeId = dfgOpNodeOut(opNodeId)
-        for (funcNodeFanoutId <- mrrgFuncFanoutId(funcNodeId)) {
-          for (valNodeFanoutId <- dfgValNodeOut(valNodeId)) {
-            // F(funcNodeId,opNodeId) == S(funcNodeFanoutId,valNodeId,valNodeFanoutId)
-            //   where funcNodeId->funcNodeFanoutId and opNodeId->valNodeId
-            val expr = ctx.mkEq(f, varS((funcNodeFanoutId, valNodeId, valNodeFanoutId)))
-            exprList.append(expr)
+    varF.foreach {
+      case ((funcNodeId, opNodeId), f) => {
+        if (dfgOpNodeOut(opNodeId) != -1) {
+          val valNodeId = dfgOpNodeOut(opNodeId)
+          for (funcNodeFanoutId <- mrrgFuncFanoutId(funcNodeId)) {
+            for (valNodeFanoutId <- dfgValNodeOut(valNodeId)) {
+              // F(funcNodeId,opNodeId) == S(funcNodeFanoutId,valNodeId,valNodeFanoutId)
+              //   where funcNodeId->funcNodeFanoutId and opNodeId->valNodeId
+              val expr = ctx.mkEq(
+                f,
+                varS((funcNodeFanoutId, valNodeId, valNodeFanoutId))
+              )
+              exprList.append(expr)
+            }
           }
         }
       }
-    }}
+    }
     exprList
   }
 
@@ -291,7 +356,10 @@ class Z3ProverMap {
     -1
   }
 
-  def omtMap(separatedPR: Boolean, scheduleControl: Boolean): ArrayBuffer[ArrayBuffer[Int]] = {
+  def omtMap(
+      separatedPR: Boolean,
+      scheduleControl: Boolean
+  ): ArrayBuffer[ArrayBuffer[Int]] = {
     val ctx = new Context() // TO-DO: provide configuration parameters
     Global.setParameter("parallel.enable", "true");
     getOmtModel(ctx, separatedPR, scheduleControl)
